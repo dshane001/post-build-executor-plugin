ff622afc993f8875fa32eaec930213dd2ad076b0
GitHub Action <action@github.com>
2023-10-12 01:26:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/dshane001/postbuildexecutorplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/dshane001/postbuildexecutorplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dshane001.postbuildexecutorplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
